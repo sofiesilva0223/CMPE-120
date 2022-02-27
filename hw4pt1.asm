c273bc36c71913be4759c58ef16b6d1570db1857
sofiesilva0223 <89554609+sofiesilva0223@users.noreply.github.com>
2022-02-26 19:34:57 -0800

    Update hw4pt1.asm

--- hw4pt1.asm
+++ hw4pt1.asm
@@ -1,24 +1,26 @@
-;Problem 1: Write a program to copy an array of type byte and size 5 into anther array of the same size and type.
+;CMPE 102
+;Problem 1: Write a program to copy an array of type byte and size 5 into another array of the same size and type.
 ;Group members: Sofia Silva,enter name, enter name
+;Source Code
 .386
 .model flat, stdcall
 .stack 4096
 ExitProcess PROTO, dwExitCode:DWORD
 .data    
-array1 BYTE 1, 2, 3, 4, 5	;array1 is the original array
-array2 BYTE 5 dup(?)		;array2 is the array that will copy the inside of array1
+list1 BYTE 1, 2, 3, 4, 5	;list1 is the original array
+list2 BYTE ?, ?, ?, ?, ?	;list2 is the array that will copy the inside of array1
 .code
 main PROC
-	mov esi, OFFSET array1
-	mov edi, OFFSET array2
-	mov al, 5				;size of 5 is stored in the al register
+	mov ecx, 5	
+	mov esi, OFFSET list1
+	mov edi, OFFSET list2
 loop1:
-	 mov ah,[esi]
-	 mov [edi],ah
-	 add esi, 2
-	 add edi, 2
-	 dec al
-jnz loop1
+	mov al,[esi]
+	mov [edi],al
+	inc esi
+	inc edi
+	dec ecx
+	jnz loop1
 	INVOKE ExitProcess,0
 main ENDP
 END main
