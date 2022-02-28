54be48cda6c5e87a28ca30b753c962108d0450de
isaackimmi <53633984+isaackimmi@users.noreply.github.com>
2022-02-27 19:52:42 -0800

    Create hwpt2.asm

---
+++ hwpt2.asm
@@ -0,0 +1,26 @@
+;CMPE 102, Lab Week-5
+;Problem 1: Write a program to copy an array of type byte and size 5 into another array of the same size and type.
+;Group members: Sofia Silva, Isaac Kim, Inderpreet Singh
+;Source Code
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess PROTO, dwExitCode:DWORD
+.data    
+list1 WORD 1, 2, 3, 4, 5	;list1 is the original array
+list2 WORD ?, ?, ?, ?, ?	;list2 is the array that will copy the inside of array1
+.code
+main PROC
+	mov ecx, 5	
+	mov esi, OFFSET list1
+	mov edi, OFFSET list2
+loop1:
+	mov ax,[esi]
+	mov [edi],ax
+	add esi, 2
+  add edi, 2
+	dec ecx
+	jnz loop1
+	INVOKE ExitProcess,0
+main ENDP
+END main
